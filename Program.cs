fe11d3acf0c95a37d7626a7dfa0a4538d5d01d83
Timmy <Timmy@DESKTOP-SL11F1M>
2022-11-01 10:38:57 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lektion5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            customer customer = new customer();
+            customer.ID = -1;
+            customer.firstname = "timmy";
+            customer.lastname = "safstrom";
+            customer.Email = "hej@gotmail.com";
+            customer customer1 = new customer();
+            customer1.ID = 3;
+            customer1.firstname = "carl";
+            customer1.lastname = "m";
+            customer1.Email = " 4";
+            customer customer2 = new customer();
+            customer2.ID = 4;
+            customer2.firstname = null;
+            customer2.lastname = null;
+            customer2.Email = null;
+
+            customer.printout(customer);
+            customer.printout(customer1);
+            customer.printout(customer2);
+            
+        }
+    }
+}
---
+++ customer.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lektion5
+{
+    public class customer
+    {
+        private int id;
+        public int ID
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    id = 0;
+                }
+                else
+                {
+                    id = value;
+                }
+            }
+        }
+        private string FirstName;
+        public string firstname
+        {
+            get
+            {
+                return FirstName;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    FirstName = "no First name";
+                }
+                else
+                {
+                    FirstName = value;
+                }
+            }
+        }
+        private string LastName;
+        public string lastname
+        {
+            get
+            {
+                return LastName;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    LastName = "No Last Name";
+                }
+                else
+                {
+                    LastName = value;
+                }
+            }
+        }
+        
+        
+        private string EMail;
+        public string Email
+        {
+            get
+            {
+                return EMail;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    EMail = "Email is empty";
+                }
+                else
+                {
+                    EMail = value;
+                }
+            }
+        }
+    public void printout(customer c1)
+        {
+            Console.WriteLine(c1.id +" "+ c1.FirstName +" " + c1.LastName + " "+ c1.EMail);
+        }
+    }
+}
